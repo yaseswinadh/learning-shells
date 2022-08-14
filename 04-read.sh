2d247b04d9f606e08ad697cc6f21ce0eaddd8806
yaseswi <yaseswi111@gmail.com>
2022-08-14 12:23:27 +0530

    varibale

---
+++ 04-read.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+read -p 'Enter your name:' name
+echo -e "Hey $name, welcome to DevOps Training"
+
+read -p 'Enter your phone number:' phone
+echo "Thanks for entering details, will text you details soon to $phone"
